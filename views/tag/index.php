cc9c3758c90abdd130a5c9508bd7cfd55ef00a4d
skoYaa <skoya85@gmail.com>
2015-06-03 02:07:10 +0200

    Promjenio gridview i umjesto text boxa sad je padajuca lista

--- views/tag/index.php
+++ views/tag/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use app\models\Tag;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\TagSearch */
@@ -10,10 +11,12 @@ use yii\grid\GridView;
 $this->title = 'Tag';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
+
 <div class="tag-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php  //echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
         <?= Html::a('Novi tag', ['create'], ['class' => 'btn btn-success']) ?>
@@ -25,9 +28,19 @@ $this->params['breadcrumbs'][] = $this->title;
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
+            
+            [       //umjesto text boxa sad je padajuca lista
+             'attribute' => 'id',
+             'filter'=>  Tag::get_status(),
+            ],
+        
+            [
+             'attribute' => 'name',
+             'filter'=>  Tag::get_status(),
+            ],
 
-            'id',
-            'name',
+            
+           
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
