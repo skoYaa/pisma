fad2cd705fc6ab9bba4d2e3513e826b784003adc
dinooo <dinooo_06@hotmail.com>
2015-06-22 01:54:48 +0200

    Ispisuje korisnike i narudbze u odgovarajucim gridviewima

--- views/admin/welcome.php
+++ views/admin/welcome.php
@@ -2,37 +2,60 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\widgets\DetailView;
+use yii\grid\GridView;
+
 /*
- * To change this license header, choose License Headers in Project Properties.
+ *  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 ?>
 
 
+
+
+
 <div class="row">
-    <div class="col-md-3">
+    <div class="col-md-7">
+        <h3>Zadnji kreirani korisnici</h3>
+        <?=
+        GridView::widget([
+            'dataProvider' => $dataProvider1,
+            //'filterModel' => $searchModel,
+            
+            'columns' => [
+                //['class' => 'yii\grid\SerialColumn'],
 
-        <h3>Novi korisnici</h3>
-
-        <?php
-        foreach ($model as $d) {?>
-            <a href="?r=account%2Fview&id=<?= $d->id; ?>"> <?php echo $d->user_name; ?></a>
-            <br>
-        <?php }
+                [ 'attribute' => 'first_name', 'label' => 'Ime'],
+                [ 'attribute' => 'last_name', 'label' => 'Prezime'],
+                [ 'attribute' => 'company_name', 'label' => 'Firma'],
+                [ 'attribute' => 'user_name', 'label' => 'Korisničko ime'],
+                
+            ],
+        ]);
         ?>
-    </div>
-    <div class="col-md-2">
 
     </div>
-    <div class="col-md-2">
 
-      
+    <div class="col-md-5">
+        <h3>Zadnje naruđbe</h3>
+        <?=
+        GridView::widget([
+            'dataProvider' => $dataProvider2,
+            //'filterModel' => $searchModel,
+            'columns' => [
+                //['class' => 'yii\grid\SerialColumn'],
+
+                [ 'attribute' => 'account_id', 'label' => 'Korisnik', 'value' => 'account.user_name'],
+                [ 'attribute' => 'package_id', 'label' => 'Paket', 'value' => 'package.name'],
+                [ 'attribute' => 'purchase_date', 'label' => 'Datum naruđbe'],
+                [ 'attribute' => 'paid', 'label' => 'Platio'],
+            ],
+        ]);
+        ?>
+
     </div>
 </div>
 
-<?php
-
-
-
-?>
+<?php ?>
