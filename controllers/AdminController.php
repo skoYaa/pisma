a22fac5669990458f8f1e9a37e6e4459eb06f231
dinooo <dinooo_06@hotmail.com>
2015-06-22 01:53:27 +0200

    Izmjena actionIndex - uzima podatke o korisnicima i narudzbama, sortira ih opadajuce po id (najnoviji ima najvisi id), i salje u view

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -10,8 +10,13 @@ use app\models\LoginForm;
 use app\models\PaymentMethod;
 use app\models\PaymentMethodSearch;
 use app\models\Account;
+use app\models\AccountSearch;
+use app\models\Purchase;
+use app\models\PurchaseSearch;
 use yii\web\NotFoundHttpException;
-use yii\data\Sort;
+
+//use yii\data\Sort;
+
 
 class AdminController extends Controller {
 
@@ -55,11 +60,18 @@ class AdminController extends Controller {
     }
 
     public function actionIndex() {
-       /******************************/
-        $korisnik = Account::find()->orderBy('id')->all();
-
-       // return $this->render('welcome', ['model' => $korisnik]);
-        /****************************/
+        /*         * *************************** */
+        //$korisnik = Account::find()->orderBy('id')->all();
+        $searchModel1= new AccountSearch();
+        $dataProvider1 = $searchModel1->search(Yii::$app->request->queryParams);
+        $dataProvider1->sort = ['defaultOrder' => ['id' => 'DESC']];
+        $dataProvider1->pagination = ['pageSize' => 5];
+        
+        $searchModel2 = new PurchaseSearch();
+        $dataProvider2 = $searchModel2->search(Yii::$app->request->queryParams);
+        $dataProvider2->sort = ['defaultOrder' => ['id' => 'DESC']];
+        $dataProvider2->pagination = ['pageSize' => 5];
+        /*         * ************************* */
 
         if (Yii::$app->user->isGuest) {
             return $this->render('index');
@@ -68,11 +80,11 @@ class AdminController extends Controller {
             if ($isAdmin == '0') { //ako nijje admin, baca ga na stranicu za frontend korisnike
                 return $this->redirect('index.php');
             }
-            //return $this->render('welcome');
-            return $this->render('welcome', ['model' => $korisnik]);
+           
+            return $this->render('welcome', ['dataProvider1' => $dataProvider1, 'dataProvider2' => $dataProvider2,]);
         }
         //return $this->render('welcome');
-        return $this->render('welcome', ['model' => $korisnik]);
+        return $this->render('welcome', ['dataProvider1' => $dataProvider1, 'dataProvider2' => $dataProvider2,]);
     }
 
     public function actionLogin() {
@@ -88,13 +100,12 @@ class AdminController extends Controller {
             if ($isAdmin == '0') { //ako nijje admin, baca ga na stranicu za frontend korisnike
                 return $this->redirect('index.php');
             }
-            
-            /*umjesto render welcome, radimo redirect na gornju akciju index,
+
+            /* umjesto render welcome, radimo redirect na gornju akciju index,
              * jer u njoj pokupi sve korisnike iz baze, a u render('welcome') to ne radi,
-             * i onda izbacje bug,jer viewu welcome ne prosljedimo korisnika*/
+             * i onda izbacje bug,jer viewu welcome ne prosljedimo korisnika */
             //return $this->render('welcome'); 
             return $this->redirect('index.php?r=admin/index');
-            
         } else {
             return $this->render('login', [
                         'model' => $model,
