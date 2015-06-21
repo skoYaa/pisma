49cae3b57f9c31ab5347308e9527750556f5febc
dinooo <dinooo_06@hotmail.com>
2015-06-21 19:24:26 +0200

    Umjesto "pozdrav", ispisuje listu korisnika. Treba ih sortirati po ID u opadajucem, jer najvisi ID ima najnoviji. Nisam to uradio

--- views/admin/welcome.php
+++ views/admin/welcome.php
@@ -1,17 +1,38 @@
 <?php
 
 use yii\helpers\Html;
-/* 
+use yii\widgets\ActiveForm;
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 ?>
 
-<div class="jumbotron">
-  <h1>Pozdrav <?php echo Yii::$app->user->identity->user_name; ?></h1>
- 
-  <p><a class="btn btn-primary btn-lg" href="http://localhost/pisma/web/index.php?r=account/index" role="button">Nastavi</a></p>
-</div> 
-        
-        
+
+<div class="row">
+    <div class="col-md-3">
+
+        <h3>Novi korisnici</h3>
+
+        <?php
+        foreach ($model as $d) {?>
+            <a href="?r=account%2Fview&id=<?= $d->id; ?>"> <?php echo $d->user_name; ?></a>
+            <br>
+        <?php }
+        ?>
+    </div>
+    <div class="col-md-2">
+
+    </div>
+    <div class="col-md-2">
+
+      
+    </div>
+</div>
+
+<?php
+
+
+
+?>
