353a69313c43937cfc80c683447a810b3a8bc44f
dinooo <dinooo_06@hotmail.com>
2015-06-22 21:44:20 +0200

    Padajuci meni u filterima za admin i nadredjenu kategoriju,+ za nadredjenu ispisuje ime kategorije a ne id. Za admina nisam znao kako uraditi

--- views/category/index.php
+++ views/category/index.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use app\models\Category;
+use app\models\Account;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\CategorySearch */
@@ -19,25 +21,38 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Nova kategorija', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?= GridView::widget([
+    <?=
+    GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-            
             'id',
-            [ 'attribute' => 'name', 'label'=>'Kategorija','format' => 'raw', 'value' => function($data){return "<a href=\"?r=category%2Fupdate&id={$data->id}\">{$data->name}</a>";}],
-            [ 'attribute' => 'description', 'label'=>'Opis'],
-            [ 'attribute' => 'parent_category', 'label'=>'Nadređena kategorija'],
-            //ispis ko je dodao ili izmjenio kategoriju, a ne ispis njegovog id broja
-            [ 'attribute' => 'account_id', 'label'=>'Dodao/izmjenio', 'value' => 'account.user_name'],
+            [ 'attribute' => 'name', 'label' => 'Kategorija', 'format' => 'raw', 'value' => function($data) {
+                    return "<a href=\"?r=category%2Fupdate&id={$data->id}\">{$data->name}</a>";
+                }],
+            [ 'attribute' => 'description', 'label' => 'Opis'],
+            [ 'attribute' => 'parent_category', 'label' => 'Nadređena kategorija',
+                'value' => function($data) { //funkcija umjesto id kategorije, ispisuje koja je kategorija
+                    $data = Category::find()
+                            ->where(['id' => $data->parent_category])
+                            ->one();
+
+                    return $data['name'];
+                },
+                'filter'=> Category::get_category_name(),
+                       
+                ],
+                    //ispis ko je dodao ili izmjenio kategoriju, a ne ispis njegovog id broja
+            [ 'attribute' => 'account_id', 'label' => 'Dodao/izmjenio', 'value' => 'account.user_name','filter'=> Category::get_admin_name()],
             //'name',
             //'description',
             //'parent_category',
             //'account_id',
-
             ['class' => 'yii\grid\ActionColumn'],
         ],
-    ]); ?>
+    ]);
+?>
 
+   
 </div>
