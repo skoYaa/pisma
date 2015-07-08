9e897a7431f5e39274895761b652f6fd0234cb49
skoYaa <skoya85@gmail.com>
2015-07-09 00:51:42 +0200

    Malo prilagodio izgled

--- views/site/about.php
+++ views/site/about.php
@@ -5,12 +5,15 @@ use yii\helpers\Html;
 $this->title = 'About';
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
+<div class="container" style="padding-top: 40px; float: left">
+    <div class="row">
+        <div class="col-md-9">
+            <ol class="breadcrumb">
+                <li><a href="index.php"><i class="fa fa-home"></i></a>
+                </li>
+                <li class="active">O nama</li>
+            </ol>
+        <p class="page-header">Informacije o nasoj kompaniji</p>
+    </div>
+    </div>
 </div>
