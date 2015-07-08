1573232155b3a16f8a2214dfcffa6375cd213983
skoYaa <skoya85@gmail.com>
2015-07-09 00:41:19 +0200

    Dodan view za kategorije

---
+++ views/site/kategorije.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+
+$this->title = 'Kategorije';
+?>
+
+<div class="container" style="padding-top: 40px; float: left">
+    <div class="row">
+        <div class="col-md-9">
+            <ol class="breadcrumb">
+                <li><a href="index.php"><i class="fa fa-home"></i></a>
+                </li>
+                <li class="active">Kategorije</li>
+            </ol></div>
+
+
+        <div class="col-lg-9">
+            <h2 class="page-header">Podkategorije</h2>
+            <div class="row">
+                <div class="col-lg-12" >
+                    <h2 class="page-header">Random pisma</h2>
+                </div>
+            </div>
+        </div>
+            
+            
+                
+                
+                </div>
+            
+            
+            </div>
+       
+   
